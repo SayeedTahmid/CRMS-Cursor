2cd4919ea945a830234ec3a3def69b5326034b7f
Sayeed Tahmid <sayeedtahmid07@gmail.com>
2025-11-02 03:26:09 +0600

    Apply secure .gitignore and remove tracked secrets

--- CRMS/backend/models/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Data models for the CRM system"""
-from models.customer import Customer
-from models.log import Log
-from models.complaint import Complaint
-from models.user import User
-
-__all__ = ['Customer', 'Log', 'Complaint', 'User']
-
-
